ec0c988206f62f9ed58419952fdbaf4f35051217
Matthew Harm Bekkema <mbekkema97@gmail.com>
2016-12-29 00:33:52 +1100

    Initial commit

---
+++ haskell-utils.hs
@@ -0,0 +1,81 @@
+--------------------------------------------------------------------------------
+-- To the extent possible under law, the author(s) have dedicated all copyright
+-- and related and neighboring rights to this software to the public domain
+-- worldwide. This software is distributed without any warranty.
+--
+-- You should have received a copy of the CC0 Public Domain Dedication along
+-- with this software. If not, see
+-- <http://creativecommons.org/publicdomain/zero/1.0/>.
+--------------------------------------------------------------------------------
+
+import Control.Monad ((>=>))
+import Data.Function (fix)
+import Data.List.NonEmpty (NonEmpty(..), (<|))
+
+
+takeLength :: [b] -> [a] -> [a]
+takeLength []    _      = []
+takeLength _     []     = []
+takeLength (_:i) (x:xs) = x : takeLength i xs
+
+compareLength :: [a] -> [b] -> Ordering
+compareLength []     []     = EQ
+compareLength []     (_:_)  = LT
+compareLength (_:_)  []     = GT
+compareLength (_:xs) (_:ys) = compareLength xs ys
+
+splitBefore :: (a -> Bool) -> [a] -> [[a]]
+splitBefore f = foldr helper []
+  where
+    helper x acc = (if f x then ([]:) else id) $ uncurry (:) $ case acc of
+      (xs:xss) -> ((x : xs), xss)
+      []       -> ([x]     , [] )
+
+insertBeforeEach :: a -> [a] -> [a]
+insertBeforeEach x = concatMap ((x:) . pure)
+
+splitRemove :: [Maybe a] -> [[a]]
+splitRemove = foldr helper []
+  where
+    helper x acc = uncurry (:) $ case x of
+      Nothing -> ([], acc)
+      Just x' -> case acc of
+        (xs:xss) -> (x' : xs, xss)
+        []       -> ([x'], [])
+
+removeSuffix :: Eq a => a         -- ^ Suffix
+                     -> [a]       -- ^ Full list
+                     -> Maybe [a] -- ^ Prefix
+removeSuffix _ []     = Nothing
+removeSuffix s [x]    = if x == s then Just [] else Nothing
+removeSuffix s (x:xs) = (x:) <$> removeSuffix s xs
+
+stripSuffix :: Eq a => [a]       -- ^ Suffix
+                    -> [a]       -- ^ Full list
+                    -> Maybe [a] -- ^ Prefix
+stripSuffix = foldr (>=>) Just . map removeSuffix . reverse
+
+stripPrefix :: Eq a => [a]       -- ^ Prefix
+                    -> [a]       -- ^ Full list
+                    -> Maybe [a] -- ^ Suffix
+stripPrefix []      s     = Just s
+stripPrefix (_:_)  []     = Nothing
+stripPrefix (x:xs) (y:ys)
+    | x == y              = stripPrefix xs ys
+    | otherwise           = Nothing
+
+showUnlinesList :: Show a => [a] -> String
+showUnlinesList []     = "[\n]"
+showUnlinesList (x:xs) = '[' : ' ' : shows x (showl xs)
+  where
+    showl []     = "\n]"
+    showl (y:ys) = '\n' : ',' : ' ' : shows y (showl ys)
+
+partitionLengths :: Integral a => [Bool] -> NonEmpty a
+partitionLengths = foldr helper (1 :| [])
+  where
+    helper True  acc     = 1 <| acc
+    helper False (a:|cc) = a+1 :| cc
+
+unfoldm :: Monad m => m (Maybe a) -> m [a]
+unfoldm x = fix $ \xs -> x >>= maybe (return []) ((<$> xs) . (:))
